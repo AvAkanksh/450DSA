75acfc42ce8a28dec2eb51689e4a1396a16c2804
AvAkanksh <ee18b002@iittp.ac.in>
2021-07-09 19:48:06 +0530

    updating files

--- Arrays01.py
+++ Arrays01.py
@@ -1,7 +1,13 @@
 def my_reverse(array):
     return array[::-1]
 
+def my_reverse2(array):
+    n = len(array)
+    for i in range(n//2):
+        array[i],array[n-1-i] = array[n-1-i],array[i]
+    return array
+
 if __name__ == '__main__':
     
-    A = [1,2,3,4,5]
-    print(my_reverse(A))
+    A = [1,2,3,4]
+    print(my_reverse2(A))
